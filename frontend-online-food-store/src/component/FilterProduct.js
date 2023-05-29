0e99613c6cc2c652a28bc56d139b5510e1740212
aliihaiderr <alihaidar.dev@gmail.com>
2023-05-29 16:07:30 +0500

    add products section and apply filter

--- frontend-online-food-store/src/component/FilterProduct.js
+++ frontend-online-food-store/src/component/FilterProduct.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { CiForkAndKnife } from "react-icons/ci";
 
-const FilterProduct = ({category}) => {
+const FilterProduct = ({category,onClick}) => {
   return (
-    <div>
-      <div className="text-3xl p-5 bg-yellow-500 rounded-full">
+    <div onClick={onClick}>
+      <div className="text-3xl p-5 bg-yellow-500 rounded-full cursor-pointer">
         <CiForkAndKnife />
       </div>
       <p className="text-center font-medium my-1 capitalize">{category}</p>
--- frontend-online-food-store/src/page/Home.js
+++ frontend-online-food-store/src/page/Home.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {useSelector} from 'react-redux'
 import Homecard from '../component/Homecard'
+import CardFeature from '../component/CardFeature'
+import {GrPrevious, GrNext} from 'react-icons/gr'
+import FilterProduct from '../component/FilterProduct'
 
 const Home = () => {
   const productData = useSelector((state) =>state.product.productList)
   console.log(productData)
   const homeProductCartList = productData.slice(1,5)
+  const homeProductCartListVegetables = productData.filter(el => el.category === 'vegetable',[])
+  console.log(homeProductCartListVegetables)
+
+  const loadingArray = new Array(4).fill(null)
+  const loadingArrayFeature = new Array(10).fill(null)
+
+
+  const slideProductRef = useRef()
+  const nextProduct =()=>{
+    slideProductRef.current.scrollLeft += 200
+  };
+  const preveProduct =()=>{
+    slideProductRef.current.scrollLeft -= 200
+  };
+
+
+  const categoryList = [...new Set(productData.map(el =>el.category))]
+  console.log(categoryList);
+
+  // filter data display
+  const [dataFilter ,setDataFilter] = useState([])
+  
+  useEffect(()=>{
+    setDataFilter(productData)
+  },[productData])
+
+
+  const handleFilterProduct = (category)=>{
+    const filter = productData.filter(el => el.category.toLowerCase() === category.toLowerCase())
+    setDataFilter(()=>{
+      return[
+        ...filter
+      ]
+    })
+  } 
+  
   return (
     <div className='p-2 md:p-4'>
       <div className='md:flex gap-4 py-2'>
@@ -20,7 +59,7 @@ const Home = () => {
         </div>
         <div className='md:w-1/2 flex flex-wrap gap-5 p-4 justify-center'>
           {
-           homeProductCartList[0] && homeProductCartList.map(el =>{
+            homeProductCartList[0] ? homeProductCartList.map(el =>{
               return(
                 <Homecard
                   key={el._id}
@@ -31,9 +70,74 @@ const Home = () => {
                 />  
               )
             })
+            :
+            loadingArray.map((el,index)=>{
+              return(
+                <Homecard
+                  key={index}
+                  loading={'Loading...'}
+                />
+              )
+            })
           }
         </div>
+      </div>
 
+
+      <div className=''>
+        <div className='flex w-full items-center'>
+          <h2 className='font-bold text-2xl text-slate-800 mb-4'>Fresh Vegetables</h2>
+          <div className='ml-auto flex gap-4'>
+              <button onClick={preveProduct} className='bg-slate-300 hover:bg-slate-400 text-lg p-1 rounded'><GrPrevious /></button>
+              <button onClick={nextProduct} className='bg-slate-300 hover:bg-slate-400 text-lg p-1 rounded'><GrNext /></button>
+          </div>
+        </div>
+        <div className='flex gap-5 overflow-scroll scrollbar-none scroll-smooth transition-all' ref={slideProductRef}>
+            {
+             homeProductCartListVegetables[0] ? homeProductCartListVegetables.map(el =>{
+                return(
+                  <CardFeature
+                      key={el._id}
+                      image = {el.image}
+                      name = {el.name}
+                      price = {el.price}
+                      category = {el.category}
+                  />
+                )
+              })
+              :
+              loadingArrayFeature.map(el => <CardFeature loading ='Loading ...' />)
+            }
+        </div>
+      </div>
+
+      <div className='my-5'>
+          <h2 className='font-bold text-2xl text-slate-800 mb-4'>Your Products</h2>
+          <div className='flex gap-4 justify-center overflow-scroll scrollbar-none'>
+              {
+                categoryList[0] && categoryList.map(el =>{
+                  return(
+                    <FilterProduct category={el} onClick={()=>handleFilterProduct(el)} />  
+                  )
+                })
+              }
+          </div>
+
+          <div className='flex flex-wrap justify-center gap-4'>
+                {
+                  dataFilter.map(el=>{
+                    return(
+                      <CardFeature 
+                        key ={el._id}
+                        image={el.image}
+                        name={el.name}
+                        price={el.price}
+                        category={el.category}
+                      />
+                    )
+                  })
+                }
+          </div>
       </div>
     </div>
   )
