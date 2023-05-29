3f7bda725415725ff17841a8a8ad785a4e2691d7
aliihaiderr <alihaidar.dev@gmail.com>
2023-05-29 15:13:24 +0500

    add loading in home card component

--- frontend-online-food-store/src/component/Homecard.js
+++ frontend-online-food-store/src/component/Homecard.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const Homecard = ({name, image, category, price}) => {
+const Homecard = ({ name, image, category, price,loading }) => {
   return (
-    <div className='bg-white shadow-md p-2'>
-        <div className='w-40 min-h-[150px]'>
-            <img src={image} alt='img' className='h-full w-full'/>
-        </div>
-        <h3 className='font-semibold text-slate-600 text-center capitalize text-lg'>{name}</h3>
-        <p className='text-center text-slate-500 font-medium'>{category}</p>
-        <p className='text-center font-bold'><span className='text-red-500'>₨</span><span> {price}</span></p>
+    <div className="bg-white shadow-md p-2 rounded min-w-[180px]">
+      {name ? (
+        <>
+          <div className="w-40 min-h-[150px]">
+            <img src={image} alt="img" className="h-full w-full" />
+          </div>
+          <h3 className="font-semibold text-slate-600 text-center capitalize text-lg">
+            {name}
+          </h3>
+          <p className="text-center text-slate-500 font-medium">{category}</p>
+          <p className="text-center font-bold">
+            <span className="text-red-500">₨</span>
+            <span> {price}</span>
+          </p>
+        </>
+      )
+      :
+      <div className="flex justify-center items-center h-full">
+        <p>{loading}</p>
+      </div>
+      
+    }
     </div>
-  )
-}
+  );
+};
 
-export default Homecard
+export default Homecard;
